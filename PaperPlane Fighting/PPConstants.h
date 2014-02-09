2d985ed209c2ef3b2e786bf34078e7e5231f2cce
istratehoratiu <istratehoratiu@yahoo.com>
2014-02-09 11:12:15 +0200

    Initial refactoring

---
+++ PaperPlane Fighting/PPConstants.h
@@ -0,0 +1,10 @@
+//
+//  PPConstants.h
+//  PaperPlane Fighting
+//
+//  Created by Horatiu Istrate on 08/02/14.
+//  Copyright (c) 2014 Horatiu Istrate. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
---
+++ PaperPlane Fighting/PPConstants.m
@@ -0,0 +1,9 @@
+//
+//  PPConstants.m
+//  PaperPlane Fighting
+//
+//  Created by Horatiu Istrate on 08/02/14.
+//  Copyright (c) 2014 Horatiu Istrate. All rights reserved.
+//
+
+#import "PPConstants.h"
